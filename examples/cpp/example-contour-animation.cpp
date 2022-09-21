8f7c367b25cafbc5ce4351f05fc8644dc5515345
Allan Leal <allanmulin@gmail.com>
2022-09-21 18:15:47 +0200

    Add example that creates a list of png contour plots

---
+++ examples/cpp/example-contour-animation.cpp
@@ -0,0 +1,80 @@
+// reaktplot - a modern C++ scientific plotting library powered by plotly
+// https://github.com/reaktplot/reaktplot
+//
+// Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+//
+// Copyright (c) 2022-2023 Allan Leal
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+// associated documentation files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute,
+// sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+// NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#include <sstream>
+
+// reaktplot includes
+#include <reaktplot/reaktplot.hpp>
+using namespace reaktplot;
+
+int main(int argc, char** argv)
+{
+    using Vec = std::vector<double>;
+    using Mat = std::vector<std::vector<double>>;
+
+    auto x = Vec{ 0.0, 1.0, 2.0, 3.0, 4.0, 5.0 };
+    auto y = Vec{ 0.0, 1.0, 2.0, 3.0, 4.0, 5.0 };
+    auto z = Mat{
+        { 10.00, 10.625, 12.500, 15.625, 20.000 },
+        { 5.625,  6.250,  8.125, 11.250, 15.625 },
+        { 2.500,  3.125,  5.000,  8.125, 12.500 },
+        { 0.625,  1.250,  3.125,  6.250, 10.625 },
+        { 0.000,  0.625,  2.500,  5.625, 10.000 }
+    };
+
+    auto z0 = Mat{
+        { NaN, NaN, NaN, NaN, NaN },
+        { NaN, NaN, NaN, NaN, NaN },
+        { NaN, NaN, NaN, NaN, NaN },
+        { NaN, NaN, NaN, NaN, NaN },
+        { NaN, NaN, NaN, NaN, NaN }
+    };
+
+    auto counter = 0;
+
+    for(auto i = 0; i < 5; ++i)
+    {
+        for(auto j = 0; j < 5; ++j)
+        {
+            Figure fig;
+
+            fig.title("CONTOUR PLOT");
+
+            fig.xaxisShowGrid(false);
+            fig.yaxisShowGrid(false);
+
+            fig.xaxisTitle("x");
+            fig.yaxisTitle("y");
+
+            z0[i][j] = z[i][j];
+
+            fig.drawContour(x, y, z0);
+
+            std::ostringstream ss;
+            ss.fill('0');
+            ss.width(3);
+            ss << counter++;
+
+            fig.save("example-contour-animation-" + ss.str() + ".png");
+        }
+    }
+}
